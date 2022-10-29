63fb431cc72bb3a064cc577a59c4d0f06ebff899
nerdfiles <nerdfiles@gmail.com>
2022-10-28 20:35:16 -0500

    docs

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-(async function () {
-  const spacy = require('spacy')
-  const nlp = spacy.load('en_core_web_sm')
-  const doc = await nlp('This is a text about Facebook.')
-  for (const ent of doc.ents) {
-    console.log(ent.text, ent.label)
-  }
-  for (const token of doc) {
-    console.log(token.text, token.pos, token.head.text)
-  }
-})()
+const spacy = require('spacy')
 
+const Phraser = () {}
+const Axiomnizer = () {}
+const Lexer = () => {}
 const Parser = () => {}
 class Doc {
-  constructor () {}
+  constructor () {
+  }
   extract () {
     return Parser()
   }
@@ -23,3 +17,18 @@ class KinglyDocument extends Doc {
 }
 class YumlDocument extends Doc {
 }
+
+(async function () {
+  const str = (process && process.argv[2]) || 'the cat is on the mat'
+  console.log(str)
+  const nlp = spacy.load('en_core_web_sm')
+  const doc = await nlp(str)
+  for (const ent of doc.ents) {
+    console.log(ent.text, ent.label)
+  }
+  for (const token of doc) {
+    console.log(token.text, token.pos, token.head.text)
+  }
+})()
+
+// EOF
